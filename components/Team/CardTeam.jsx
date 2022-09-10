4800034cd22eb98e7a620e0924cf3368dff367d6
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-09-10 20:45:24 +0300

    feat: add img for cardTeam

--- components/Team/CardTeam.jsx
+++ components/Team/CardTeam.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import NextImage from '../../../reusable/Image';
-// import women from '../../../../public/woman-1.jpg';
+import NextImage from '../reusable/Image';
+import women from '../../public/woman.jpg';
 
 const CardTeam = ({ team }) => {
   //   console.log('team', team);
   return (
     <div>
-      {/* <NextImage src={women} className="rounded" layout="fill" /> */}
+      <NextImage src={women} className="rounded" layout="fill" />
       <h3 className="mt-4 uppercase">{team.name}</h3>
       <p className="mt-2">{team.prof}</p>
     </div>
