f634cdb106d9f538ebdce74bcd67a4b202d7b67a
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-09-10 20:38:13 +0300

    feat: add component Heading

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -1,16 +1,15 @@
 // import CardTeam from './CardTeam';
-// import Heading from '../../Heading';
+import Heading from '../reusable/Heading';
 
 const Team = ({ teams }) => {
   // console.log('teams', teams);
   return (
     <div className="py-10">
-      {/* <Heading
+      <Heading
         tag="h2"
         text={teams.title}
         className="uppercase font-semibold text-[28px] leading-[33px] mb-6 text-black"
-      /> */}
-      <h2>{teams.title}</h2>
+      />
       <p className="mb-14 w-[464px] text-blue-800">{teams.description}</p>
       {/* <ul className="grid grid-cols-4 gap-4">
         {teams &&
