1755a0af74694ce96bcad7153f41f89b3abc1091
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2022-09-19 16:06:23 +0300

    fix: add cloudinary and corrected team section

--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -1,27 +1,33 @@
+import Section from 'components/reusable/Section';
+import Container from 'components/reusable/Container';
 import CardTeam from './CardTeam';
 import Heading from '../reusable/Heading';
 
-const Team = ({ teams, listCard }) => {
-  // console.log('listCard', listCard);
+const Team = ({ teams }) => {
+  // console.log('listCard', teams);
   return (
-    <div className="pt-8 pb-16 text-main md:pt-16 md:pb-32 lg:pt-[124px] lg:pb-[248px]">
-      <Heading
-        tag="h2"
-        text={teams.title}
-        className="uppercase font-semibold text-[28px] leading-[33px] mb-6"
-      />
-      <p className="mb-4 text-base font-semibold text-brand1 md:w-[464px] md:mb-14">
-        {teams.description}
-      </p>
-      <ul className="grid grid-cols-2 gap-x-3 gap-y-4 md:gap-x-5 lg:grid-cols-4 lg:gap-x-4">
-        {listCard &&
-          listCard.teams.data.map(team => (
-            <li key={team.id}>
-              <CardTeam team={team} />
-            </li>
-          ))}
-      </ul>
-    </div>
+    <Section>
+      <Container>
+        <div className="pt-8 pb-16 text-main md:pt-16 md:pb-32 lg:pt-[124px] lg:pb-[248px]">
+          <Heading
+            tag="h2"
+            text={teams.title}
+            className="uppercase font-semibold text-[28px] leading-[33px] mb-6"
+          />
+          <p className="mb-4 text-base font-semibold text-brand1 md:w-[464px] md:mb-14">
+            {teams.description}
+          </p>
+          <ul className="grid grid-cols-2 gap-x-3 gap-y-4 md:gap-x-5 lg:grid-cols-4 lg:gap-x-4">
+            {teams &&
+              teams.teams.data.map(team => (
+                <li key={team.id}>
+                  <CardTeam team={team} />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </Container>
+    </Section>
   );
 };
 export default Team;
