24212c13f554bb20ccf4374666d1733ee7f714b6
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-09-22 03:09:40 +0300

    feat: add styles for projects sections

--- components/projects/BenefitsBlock.jsx
+++ components/projects/BenefitsBlock.jsx
@@ -1,7 +1,14 @@
+import Container from '../reusable/Container';
 import SectionType1 from '../sections/SectionType1';
 
 const BenefitsBlock = ({ data }) => {
-  return <SectionType1 data={data.section} />;
+  return (
+    <section>
+      <Container className="project-container">
+        <SectionType1 data={data.section} />
+      </Container>
+    </section>
+  );
 };
 
 export default BenefitsBlock;
